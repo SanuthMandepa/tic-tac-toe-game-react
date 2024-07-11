22cb3e35cb66543fbb0cf2d18c2e8de2e0cb12f4
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-07-11 14:01:08 +0530

    return 9 buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ export default function Square() {
     <>
       <button className="square">X</button>
       <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
     </>
   );
 }
